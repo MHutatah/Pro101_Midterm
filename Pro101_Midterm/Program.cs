74c04643f14c00a2cf2079b8855fb940965f4660
MHutatah <49297612+MHutatah@users.noreply.github.com>
2024-11-25 10:57:29 +0300

    First Question Answer

---
+++ Pro101_Midterm/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Pro101_Midterm;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Student Mohammed = new()
+        {
+            Name = "Mohammed",
+            Age = 28,
+            Grade = "Exceptional"
+        };
+        Student Ahmed = new()
+        {
+            Name = "Ahmed",
+            Age = 22,
+            Grade = "Excellent"
+        };
+        Student Ali = new()
+        {
+            Name = "Ali",
+            Age = 25,
+            Grade = "Good"
+        };
+
+        Mohammed.DisplayInfo();
+        Ahmed.DisplayInfo();
+        Ali.DisplayInfo();
+    }
+}
+
+class Student
+{
+    public required string Name;
+    public required int Age;
+    public required string Grade;
+
+    public void DisplayInfo(){
+        Console.WriteLine($"Name: {this.Name}, Age: {this.Age}, Grade: {this.Grade}");
+    }
+}
